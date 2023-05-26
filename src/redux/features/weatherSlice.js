0fa29f03b40449b281cd3e03b006f3943c07f7cb
Lul-Abdifan <tasanbannu@gmail.com>
2023-05-26 09:49:02 -0400

    Update API_URL

--- src/redux/features/weatherSlice.js
+++ src/redux/features/weatherSlice.js
@@ -1,62 +1,58 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import axios from 'axios';
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 
-// const API_URL = 'https://restcountries.com/v3.1/region/Africa';
-const API_KEY = 'c7b015b39d5fdfefbb455c135bc60041';
-
-// Define the asynchronous thunk for fetching weather data
+const API_URL = "https://restcountries.com/v3.1/region/Africa";
+const API_KEY = "c7b015b39d5fdfefbb455c135bc60041";
 
 export const fetchCountryData = createAsyncThunk(
-  'Country/countryData',
+  "Country/countryData",
   async () => {
-    const countryResponse = await axios.get(
-      'https://restcountries.com/v3.1/region/Africa',
-    );
+    const countryResponse = await axios.get(API_URL);
     return countryResponse.data;
-  },
+  }
 );
 export const forecastWeatherData = createAsyncThunk(
-  'weather/forecastWeatherData',
+  "weather/forecastWeatherData",
   async (capital) => {
     const forecastApiURL = `https://api.openweathermap.org/data/2.5/forecast?q=${capital}&appid=${API_KEY}`;
 
     const forecastResponse = await axios.get(forecastApiURL);
     return forecastResponse.data;
-  },
+  }
 );
 
 // Create the weather slice
 const weatherSlice = createSlice({
-  name: 'weather',
+  name: "weather",
   initialState: {
     data: [],
-    forecast: '',
-    status: 'idle',
+    forecast: "",
+    status: "idle",
     error: null,
   },
   reducers: {},
   extraReducers: (builder) => {
     builder
       .addCase(fetchCountryData.pending, (state) => {
-        state.status = 'loading';
+        state.status = "loading";
       })
       .addCase(fetchCountryData.fulfilled, (state, action) => {
-        state.status = 'succeeded';
-        state.data = action.payload; // Assign the fetched weather data array to the state
+        state.status = "succeeded";
+        state.data = action.payload; // 
       })
       .addCase(fetchCountryData.rejected, (state, action) => {
-        state.status = 'failed';
+        state.status = "failed";
         state.error = action.error.message;
       })
       .addCase(forecastWeatherData.pending, (state) => {
-        state.status = 'loading';
+        state.status = "loading";
       })
       .addCase(forecastWeatherData.fulfilled, (state, action) => {
-        state.status = 'succeeded';
+        state.status = "succeeded";
         state.forecast = action.payload; // Assign the fetched weather data array to the state
       })
       .addCase(forecastWeatherData.rejected, (state, action) => {
-        state.status = 'failed';
+        state.status = "failed";
         state.error = action.error.message;
       });
   },
